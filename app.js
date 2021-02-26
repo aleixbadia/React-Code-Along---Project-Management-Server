ec6b29822e78b86b61696dedca0e08a87d8165b1
Aleix Badia <aleix.284@gmail.com>
2021-02-26 16:46:00 +0100

    done

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const cors = require('cors');
 require("dotenv").config();
 
 const app = express();
 
-// const projectRouter = require('./routes/project.router');
-// const taskRouter = require('./routes/task.router');
+const projectRouter = require('./routes/project.router');
+const taskRouter = require('./routes/task.router');
 // const router = require('./routes/index');
 
 // MONGOOSE CONNECTION
@@ -34,10 +35,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // CORS SETTINGS (MIDDLEWARE) TO ALLOW CROSS-ORIGIN INTERACTION:
-//  ...
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000'] // <== this will be the URL of our React app (it will be running on port 3000)
+}));
 
 // ROUTES MIDDLEWARE:
-//  app.use('/api', projectRouter);
-//  app.use('/api', taskRouter);
+ app.use('/api', projectRouter);
+ app.use('/api', taskRouter);
 
 module.exports = app;
--- routes/task.router.js
+++ routes/task.router.js
@@ -1,22 +1,95 @@
 //      routes/task-routes.js
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
 
-
-
-
-// GET '/api/tasks/:id'   => to retrieve a specific task
-
-
-
+const Project = require("../models/project.model");
+const Task = require("../models/task.model");
 
 // POST '/api/tasks'      => to create a new task
+router.post("/tasks", (req, res) => {
+  const { title, description, projectId } = req.body;
 
+  Task.create({
+    title,
+    description,
+    project: projectId,
+  })
+    .then((newTaskDocument) => {
+      Project.findByIdAndUpdate(
+        projectId,
+        {
+          $push: { tasks: newTaskDocument._id },
+        },
+        { new: true }
+      )
+        .then((updatedProject) => {
+          res.status(201).json(updatedProject);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
+// GET '/api/tasks/:id'   => to retrieve a specific task
+router.get("/tasks/:id", (req, res) => {
+  const { id } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res
+      .status(400) //  Bad Request
+      .json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Task.findById(id)
+    .then((foundTask) => {
+      res.status(200).json(foundTask); // OK
+    })
+    .catch((err) => {
+      res.status(500).json(err); // Internal Server Error
+    });
+});
 
 // PUT '/api/tasks/:id'    => to update a specific task
+router.put("/tasks/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
 
-
+  Task.findByIdAndUpdate(id, { title, description }, { new: true })
+    .then((updatedTask) => {
+      res.status(200).json(updatedTask);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
 // DELETE '/api/tasks/:id'     => to delete a specific task
+router.delete("/tasks/:id", (req, res) => {
+  const { id } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Task.findByIdAndRemove(id)
+    .then(() => {
+      res.status(202).send(`Document ${id} was removed successfully.`);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
